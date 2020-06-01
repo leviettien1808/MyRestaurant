49c22fa3b918963a7d5e47ec5e8991519989c1f6
Tien1808 <leviettien1808@gmail.com>
2020-06-01 14:51:29 +0700

    initial js folder

---
+++ js/myjs.js
@@ -0,0 +1,33 @@
+
+// Open the Modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+    document.getElementById("myMenu").style.display = "none";
+    document.getElementById("myTitle").style.display = "none";
+}
+// Close the Modal
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+    document.getElementById("myMenu").style.display = "block";
+    document.getElementById("myTitle").style.display = "block";
+}
+var slideIndex = 1;
+showSlides(slideIndex);
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
